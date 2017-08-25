257f881e15d259da3b91840cdce0ac6771ba3572
Phuriwat <phuriwat.s@dotography.com>
2017-08-25 15:56:04 +0700

    move file to repo

---
+++ src/features/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-wrapper">
+        <a href="#" className="header-btn">Pay Bill</a>
+      </div>
+    );
+  }
+}
+
+export default Header;
